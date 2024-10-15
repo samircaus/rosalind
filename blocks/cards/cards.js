f511012b087796593dd831a0553d95f8603ab92f
Samir Caus <scaus@adobe.com>
2024-10-15 16:15:14 +0200

    lint fix

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,18 +6,18 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
-    const targetAnchor = li.querySelector("a");
+    const targetAnchor = li.querySelector('a');
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else {
         div.className = 'cards-card-body';
 
         if (targetAnchor) {
-          const wrapperAnchor = document.createElement("a");
+          const wrapperAnchor = document.createElement('a');
           wrapperAnchor.href = targetAnchor.href;
           wrapperAnchor.innerHTML = div.innerHTML;
 
-          div.innerHTML = ""
+          div.innerHTML = '';
           div.appendChild(wrapperAnchor);
         }
       }
